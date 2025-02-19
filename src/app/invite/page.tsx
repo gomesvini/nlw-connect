fe423877e7d4560f1dc6299c8e9f03522d17a414
Vinicius Gomes <pain-nici2011@live.com>
2025-02-19 16:02:18 -0300

    visual part completed

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -4,6 +4,9 @@ import { IconButton } from '@/components/icon-button'
 import { InputField, InputIcon, InputRoot } from '@/components/input'
 import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react'
 import logo from '../../assets/logo.svg'
+import cooper from '../../assets/medal-cooper.svg'
+import gold from '../../assets/medal-gold.svg'
+import silver from '../../assets/medal-silver.svg'
 
 export default function InvitePage() {
   return (
@@ -56,9 +59,9 @@ export default function InvitePage() {
                 Acessos ao link
               </span>
 
-              <MousePointerClick className='size-5 text-purple absolute top-3 left-3'/>
+              <MousePointerClick className="size-5 text-purple absolute top-3 left-3" />
             </div>
-            
+
             <div className=" relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
               <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
                 875
@@ -67,7 +70,7 @@ export default function InvitePage() {
                 Incrições feitas
               </span>
 
-              <BadgeCheck className='size-5 text-purple absolute top-3 left-3'/>
+              <BadgeCheck className="size-5 text-purple absolute top-3 left-3" />
             </div>
 
             <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
@@ -78,8 +81,64 @@ export default function InvitePage() {
                 Posição no ranking
               </span>
 
-              <Medal className='size-5 text-purple absolute top-3 left-3'/>
+              <Medal className="size-5 text-purple absolute top-3 left-3" />
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full max-w-[440px] space-y-5">
+        <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+          Ranking de indicações
+        </h2>
+
+        <div className="space-y-4">
+          <div className=" relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">1°</span> | Vinícius Gomes
+            </span>
+
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1128
+            </span>
+
+            <Image
+              src={gold}
+              alt="medalha de ouro"
+              className="absolute top-0 right-8"
+            />
+          </div>
+
+          <div className=" relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">2°</span> | Joyce Ferreira
+            </span>
+
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              928
+            </span>
+
+            <Image
+              src={silver}
+              alt="medalha de prata"
+              className="absolute top-0 right-8"
+            />
+          </div>
+
+          <div className=" relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">3°</span> | Diego Fernandes
+            </span>
+
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              875
+            </span>
+
+            <Image
+              src={cooper}
+              alt="medalha de bronze"
+              className="absolute top-0 right-8"
+            />
           </div>
         </div>
       </div>
